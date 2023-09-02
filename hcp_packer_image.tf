db4ad1061637668b7b59bd33af8c57989bac31be
Richard Annand <richard+git@ohheyrj.co.uk>
2023-09-02 10:59:47 +0100

    feat: added ami for docker host and nat instance

--- hcp_packer_image.tf
+++ hcp_packer_image.tf
@@ -12,6 +12,20 @@ data "hcp_packer_image" "php8-2" {
   region         = "eu-west-2"
 }
 
+data "hcp_packer_image" "docker_host" {
+  bucket_name    = "docker-host"
+  channel        = "latest"
+  cloud_provider = "aws"
+  region         = "eu-west-2"
+}
+
+data "hcp_packer_image" "nat_instance" {
+  bucket_name    = "nat-instance"
+  channel        = "latest"
+  cloud_provider = "aws"
+  region         = "eu-west-2"
+}
+
 output "ami_php81" {
   value       = data.hcp_packer_image.php8-1.cloud_image_id
   description = "PHP 8.1 image ID"
@@ -20,4 +34,14 @@ output "ami_php81" {
 output "ami_php82" {
   value       = data.hcp_packer_image.php8-2.cloud_image_id
   description = "PHP 8.2 image ID"
+}
+
+output "ami_docker_host" {
+  value       = data.hcp_packer_image.docker_host.cloud_image_id
+  description = "Docker host image ID"
+}
+
+output "ami_nat_instance" {
+  value       = data.hcp_packer_image.nat_instance.cloud_image_id
+  description = "NAT instance image ID"
 }
